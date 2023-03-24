6deca7dff4c5c374b7ab48257a1f664cf7887281
kfggww <kfggww@outlook.com>
2023-03-24 17:27:37 +0800

    Test link list.

--- chapter1.2/test-list.c
+++ chapter1.2/test-list.c
@@ -1,18 +1,45 @@
 #include <stdio.h>
 #include "list.h"
 
+void print_list(list_t *list) {
+    list_foreach(list, node) {
+        printf("%d%s", node->data, node->next != list->head ? ", " : "\n");
+    }
+    printf("size: %d\n", list->size);
+}
+
 int main(int argc, char const *argv[]) {
 
-    list_t *l1 = list_create();
+    /*1. create*/
+    list_t *list = list_create();
 
-    for (int i = 0; i < 16; i++) {
-        list_append(l1, i);
+    /*2. append*/
+    for (int i = 0; i < 8; i++) {
+        list_append(list, i);
     }
+    printf("list_append:\n");
+    print_list(list);
 
-    printf("size: %d\n", l1->size);
+    /*3. prepend*/
+    list_prepend(list, 1024);
+    printf("\nlist_prepend:\n");
+    print_list(list);
 
-    list_foreach(l1, node) { printf("%d\n", node->data); }
+    /*4. search*/
+    int data = 4;
+    int found = list_search(list, data);
+    printf("\nlist_search %d: %s\n", data,
+           found == SUCCESS ? "FOUND" : "NOT FOUND");
+    print_list(list);
 
-    list_free(l1);
+    /*5. delete*/
+    data = 5;
+    list_delete(list, data);
+    printf("\nlist_delete %d\n", data);
+    print_list(list);
+
+    /*6. free*/
+    list_free(list);
+
     return 0;
 }
